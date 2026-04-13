8aa0fdad82344c3de693d307b6b17c9309180046
Theriault Benoit <257194341+theriaubenoit-ops@users.noreply.github.com>
2026-04-13 15:15:14 -0400

    Update registry.py

--- src/registry.py
+++ src/registry.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-aNA v5.2 - The Centralized Genome of aNA
+aNA v5.3b - The Centralized Genome of aNA
 Description: This module acts as the organism's genetic fingerprint. It catalogs and initializes all the "Organs" (Thalamus, Hippocampus, Cortex), enabling consistent instantiation and seamless communication between subcortical and cortical systems. Without this registry, the organism loses its unified structure and its ability to maintain global homeostasis.
 
-aNA v5.2 - Le Génome Centralisé d'aNA (FR)
+aNA v5.3b - Le Génome Centralisé d'aNA (FR)
 Description :  Ce module agit comme l'empreinte génétique de l'organisme. Il répertorie et initialise l'ensemble des "Organes" (Thalamus, Hippocampe, Cortex) permettant une instanciation cohérente et une communication fluide entre les systèmes subcorticaux et corticaux. Sans ce registre, l'organisme perd sa structure unifiée et sa capacité à maintenir une homéostasie globale.
 
 Architecture, concept and supervision: Benoit Theriault
